c37baf816c67b63c062de2b22ef356b5b88f3959
Dêner Ribeiro <dener_ribeiro13@hotmail.com>
2019-11-10 20:02:38 -0300

    Update ConnectionFactory.java

--- src/factory/ConnectionFactory.java
+++ src/factory/ConnectionFactory.java
@@ -1,16 +1,5 @@
-/*
-Nome: Bruno Domingues, Dêner Ribeiro, Gustavo Veronez, Tayane Lopes.
-Turma: TI-167.
-Descrição: Sistema para fazer cadastro em banco de dados das contas à pagar da empresa.
-Data de Termino: 24/08/2015.
-
-*/
 package factory;
 
-/**
- *
- * @author GRUPO 2 JSE1
- */
 import java.sql.*;
 public class ConnectionFactory {
     //faz a conexão com o banco de dados
