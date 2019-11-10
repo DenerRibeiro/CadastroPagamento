acce41f398c1aba9fb30792ceb9616b12baeb785
Dêner Ribeiro <dener_ribeiro13@hotmail.com>
2019-11-10 20:03:04 -0300

    Update TestConnection.java

--- src/factory/TestConnection.java
+++ src/factory/TestConnection.java
@@ -1,16 +1,6 @@
-/*
-Nome: Bruno Domingues, Dêner Ribeiro, Gustavo Veronez, Tayane Lopes.
-Turma: TI-167.
-Descrição: Sistema para fazer cadastro em banco de dados das contas à pagar da empresa.
-Data de Termino: 24/08/2015.
 
-*/
 package factory;
 
-/**
- *
- * @author GRUPO 2 JSE1
- */
 import java.sql.Connection;
 import java.sql.SQLException;
 public class TestConnection {
